ea6d5758b3f3853fcef970ec3ef32df45c4c589d
aluno <aluno@ADNOBRE.local>
2024-05-23 15:54:04 -0300

    four commit

--- src/pages/Doacao.jsx
+++ src/pages/Doacao.jsx
@@ -7,7 +7,7 @@ import abacaxi from "../assets/img/abacaxi.svg"
 import moeda from "../assets/img/moeda.svg"
 import roupa from "../assets/img/roupa.svg"
 import { Footer } from '../components/footer'
-import Carteira from '../assets/img/carteira.gif'
+
 
 export const Doacao = () => {
   return (
@@ -15,7 +15,7 @@ export const Doacao = () => {
     <Header  />
 
     <section className='sec-quem-somos'>
-        <div className="quem-somos">
+        <div className="layer">
                     <div className="linha-1">
                         <img src={Retangulo} />
                     </div>
@@ -47,7 +47,7 @@ export const Doacao = () => {
                 </div>
             </div>
     </section>
-    < Footer />
+    
 
     
     </>
