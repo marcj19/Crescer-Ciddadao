f4e851062ec505b18a9aa890eacc6a13f4969075
marcj19 <paulomjunior7@gmail.com>
2024-06-09 12:08:21 -0300

    mudanca de layers

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,6 +6,7 @@ import iconPsicologia from "../assets/img/iconPsicologia.png";
 import footerimgI from "../assets/img/footerimgI.png";
 import footerimgW from "../assets/img/footerimgW.png";
 import footerimgY from "../assets/img/footerimgY.png";
+import { Footer } from "./footer"
 
 const Cards = () => {
   const [showFisioterapia, setShowFisioterapia] = useState(false);
@@ -52,33 +53,8 @@ const Cards = () => {
         <button className="button"  onClick={() => setShowPsicologia(!showPsicologia)}> Leia Mais </button>
       </div>
     </div>
-    <footer className='footer'>
-      <div className='footertxtmain'>
-      <div className='footertxt1'>
-        <p>Contato:</p>
-        <p>crescercidadao@gmail.com</p>
-        <p>3224-8975</p>
-      </div>
-      <div className='footertxt2'>
-        <p>Endereço:</p>
-        <p>R.São Domingos, 76 - Santa Monica, Feira de Santana</p>
-        <p>BA, 44050-081</p>
-      </div>
-      </div>
     
-      <div className='imgfooter'> 
-      <a href="https://www.whatsapp.com/" target="_blank" rel="noopenner noreferrer">
-      <img src={footerimgW} alt='footerW'/>
-      </a>
-      <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-      <img src={footerimgI} alt='footerI'/>
-      </a>
-      <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
-      <img src={footerimgY} alt='footerY'/>
-      </a>
-      </div> 
-    </footer>
-
+    <Footer />
     </>
   );
 };
