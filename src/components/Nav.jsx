5567d140a032444b5682048e93c9d327eecb9539
marcj19 <paulomjunior7@gmail.com>
2024-06-13 02:40:51 -0300

    blog finalizado

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import './nav.css';
 import { Link } from "react-router-dom";
+
+
+
 function Nav(){
 
     return(
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,42 +1,50 @@
 import React from 'react';
 import './Blog.css'; 
+import Header from '../components/Header'
+import Nav from '../components/Nav'
+import { Footer } from '../components/footer'
+import Retangulo from "../assets/img/retangulo-1.svg"
+import Retangulo2 from "../assets/img/retangulo-2.svg"
+import Compartilhar from '../assets/img/Compartilhar.svg'
+import Instagram from "../assets/img/Icon-color.svg"
+import WhatsApp from "../assets/img/Vector.svg"
+import Gincana from '../assets/img/GINCANA.jpg'
 
-const Blog = () => {
-  return (
-    <nav className="navbar">
-      <ul className="navbar-list">
-        <li className="navbar-item">
-          <a href="/perfil" className="navbar-link">Perfil</a>
-        </li>
-        <li className="navbar-item">
-          <a href="/seguranca" className="navbar-link">Segurança</a>
-        </li>
-        <li className="navbar-item">
-          <a href="/privacidade" className="navbar-link">Privacidade</a>
-        </li>
-        <li className="navbar-item">
-          <a href="/blog" className="navbar-link">Blog</a>
-        </li>
-        <li className="navbar-item">
-          <a href="/crescer-cidadao" className="navbar-link">Crescer Cidadão</a>
-        </li>
-        <li className="navbar-item">
-          <a href="/servicos" className="navbar-link">Serviços</a>
-        </li>
-        <li className="navbar-item">
-          <a href="/" className="navbar-link">Início</a>
-        </li>
-        <li className="navbar-item">
-          <a href="/sobre-nos" className="navbar-link">Sobre-Nós</a>
-        </li>
-        <li className="navbar-item">
-          <a href="/carregue-sua-imagem" className="navbar-link">Carregue Sua Imagem</a>
-        </li>
-        <li className="navbar-item">
-          <a href="/apoie-nos" className="navbar-link">Apoie-Nos</a>
-        </li>
-      </ul>
-    </nav>
-  );
-};
-export default Blog
+
+export default function Blog(){
+  return(
+    <>
+    <Header/>
+    <Nav/>
+    <div className="quem-somos">
+                <div className="linha">
+                    <img src={Retangulo} />
+                </div> 
+                <div className="title">
+                    <h2>BLOG</h2>
+                </div>
+                <div className="linha">
+                    <img src={Retangulo2} />
+                </div>
+            </div>
+    <section className="Eventos">
+      <div className="layout-inside">
+        <div className='image-layout-event'>
+          <img src={Gincana} alt="" />
+          </div>
+          <div className="both">
+        <p className="text-event">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates earum veniam quae repellendus delectus perferendis fugiat cumque perspiciatis aliquam numquam adipisci, reiciendis aspernatur dignissimos ab animi accusantium dolor? Eaque!
+        </p>
+        <div className="layout-link">
+          <img src={Compartilhar} alt="" />
+          <img src={Instagram} alt="" />
+          <img src={WhatsApp} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <Footer/>
+    </>
+  )
+}
